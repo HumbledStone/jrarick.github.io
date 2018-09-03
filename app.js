6bdb6899670c97773ebb3865638170701a3bbe97
jrarick <36737588+jrarick@users.noreply.github.com>
2018-09-02 21:18:40 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+// Event handler for checkbox at top of page
 $('#night-mode').change(function() {
+  // Apply night styles if checkbox is checked
   if ($('#night-mode').is(':checked')) {
     $('.banner').removeClass('day-banner').addClass('night-banner');
     $('body').addClass('night-background');
     $('.project-div').removeClass('day-project-div').addClass('night-project-div');
     $('.sub-header').removeClass('day-sub-header').addClass('night-sub-header');
     $('.contact-item').removeClass('day-contact-item').addClass('night-contact-item');
-  } else {
+  }
+  // Apply day (default) styles if checkbox is unchecked
+  else {
     $('.banner').removeClass('night-banner').addClass('day-banner');
     $('body').removeClass('night-background');
     $('.project-div').removeClass('night-project-div').addClass('day-project-div');
